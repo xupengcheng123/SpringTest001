07d7bfbb7ebaa57e6d97761ab257b362e1d6bd03
xupengcheng123 <53596319@qq.com>
2019-07-01 00:00:51 +0800

    ceshi

---
+++ src/sql/myschool001.sql
@@ -0,0 +1,38 @@
+/*
+SQLyog v10.2 
+MySQL - 5.5.27 : Database - myschool
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`myschool` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `myschool`;
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `stuid` int(11) NOT NULL AUTO_INCREMENT,
+  `stuname` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`stuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`stuid`,`stuname`,`age`,`address`) values (1,'张三',18,'来广营'),(2,'李四',19,'北京'),(3,'xpc',20,'上海');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
